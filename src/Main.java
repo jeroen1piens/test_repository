6e5b59e63c9384471fb0fdb18d12a5ad1bf16a0a
jeroen1piens <jeroen.piens@outlook.com>
2024-09-11 21:14:37 +0200

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world in which we live bla bla bal!");
         int a = 5;
         int b = 3;
         System.out.println(a-b);
+        String str = "Hello";
+        System.out.println(str);
     }
 }
